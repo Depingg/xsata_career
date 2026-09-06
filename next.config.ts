fef474458c3ac61809fd7a4785a37160e6fe90df
imherebr0 <kepinsekolah02@gmail.com>
2026-09-06 11:19:39 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* isi konfigurasi proyek Anda di sini jika ada */
 };
 
 export default nextConfig;
